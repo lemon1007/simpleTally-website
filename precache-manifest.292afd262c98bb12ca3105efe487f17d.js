3a0fddf3b34bab9dc663702bc3d1fafd2c373719
青砚 <lemon19961007@sina.com>
2023-01-18 01:07:10 +0800

    deploy

---
+++ precache-manifest.292afd262c98bb12ca3105efe487f17d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380e6d8f0378af2158ec",
+    "url": "/simpleTally-website/css/app.2e1b09ec.css"
+  },
+  {
+    "revision": "60e53082fddbc02f0552e7815e2e4d84",
+    "url": "/simpleTally-website/index.html"
+  },
+  {
+    "revision": "380e6d8f0378af2158ec",
+    "url": "/simpleTally-website/js/app.4f1284ad.js"
+  },
+  {
+    "revision": "3caa5da3939bdebffc76",
+    "url": "/simpleTally-website/js/chunk-vendors.9b885fcf.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/simpleTally-website/manifest.json"
+  }
+]);
